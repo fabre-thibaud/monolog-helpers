9199b2ceaaa3752cb7ddd77e97e20363043dc8b1
Thibaud Fabre <thibaud@idgarages.com>
2019-01-04 16:51:46 +0100

    Improve context and extra formatting

--- src/Processor/GelfContextProcessor.php
+++ src/Processor/GelfContextProcessor.php
@@ -4,9 +4,20 @@ namespace TFabre\Monolog\Processor;
 
 class GelfContextProcessor
 {
+	
+	private $channel;
+	
+	public function __construct(int $channel = 23)
+	{
+		$this->channel = $channel;
+	}
+	
     public function __invoke(array $record): array
     {
         $record['context'] = [ 'context' => $record['context'] ];
+        $record['extra'] = [ 'extra_json' => array_merge(['channel' => $record['channel']], $record['extra'])];
+
+        $record['channel'] = $this->channel;
 
         return $record;
     }
